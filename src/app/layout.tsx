8b8f91506e1923a1f384f87c9b9e87f8981026b7
unhiredcoder <proboys777333@gmail.com>
2024-08-04 17:08:29 +0530

    Some changes in env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ import { Toaster } from 'react-hot-toast';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Live Coder",
+  title: "Live Devs",
   description: "An application to help coding with random devs online ",
 };
 
